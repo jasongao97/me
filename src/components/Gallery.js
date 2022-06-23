1c3782eb01aaf32d9f38841a1183d3f0eaaf8200
jasongao97 <jasongao97@gmail.com>
2022-06-22 20:32:44 -0400

    update photo gallery logic

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,28 +5,25 @@ import "react-medium-image-zoom/dist/styles.css";
 
 const Gallery = ({ photos }) => {
   return (
-    <>
-      <h2>Photos</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4" id="gallery">
-        {photos.map((photo, index) => {
-          const image = photo.node.childImageSharp.gatsbyImageData;
-          return (
-            <div
-              className="aspect-[4/3] rounded overflow-hidden flex justify-center items-center"
-              key={index}
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4" id="gallery">
+      {photos.map((photo, index) => {
+        const image = photo.node.childImageSharp.gatsbyImageData;
+        return (
+          <div
+            className="aspect-[4/3] rounded overflow-hidden flex justify-center items-center"
+            key={index}
+          >
+            <Zoom
+              wrapStyle={{
+                objectFit: "cover",
+              }}
             >
-              <Zoom
-                wrapStyle={{
-                  objectFit: "cover",
-                }}
-              >
-                <GatsbyImage image={image} className="h-full" alt="Mirror" />
-              </Zoom>
-            </div>
-          );
-        })}
-      </div>
-    </>
+              <GatsbyImage image={image} className="h-full" alt="Mirror" />
+            </Zoom>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
